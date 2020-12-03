327a815a3ac4eba680dae856bcdf8ed7b74760dd
Hans <hans.schuelein@stud-mail.uni-wuerzburg.de>
2020-12-03 10:26:15 +0100

    day 3

--- 02.py
+++ 02.py
@@ -9,7 +9,9 @@ def main() -> None:
         if line:
             password_format, password = line.split(": ")
             amounts, character = password_format.split(" ")
-            minimal_count, maximal_count = [int(c) for c in amounts.split("-")]
+            minimal_count, maximal_count = [
+                int(number) for number in amounts.split("-")
+            ]
             if minimal_count <= password.count(character) <= maximal_count:
                 count += 1
     print(count)
@@ -19,7 +21,9 @@ def main() -> None:
         if line:
             password_format, password = line.split(": ")
             positions, character = password_format.split(" ")
-            first_position, last_position = [int(c) - 1 for c in positions.split("-")]
+            first_position, last_position = [
+                int(number) - 1 for number in positions.split("-")
+            ]
             if len(password) > last_position and (
                 (password[first_position] == character)
                 ^ (password[last_position] == character)
---
+++ 03.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import aoc
+
+
+def main() -> None:
+    board = [line for line in aoc.getstr(3).split("\n") if line]
+
+    def is_tree(x: int, y: int) -> bool:
+        return board[y][x % len(board[0])] == "#"
+
+    def count_trees(dx: int, dy: int) -> int:
+        x = 0
+        y = 0
+        tree_count = 0
+        while 1:
+            x += dx
+            y += dy
+            if y >= len(board):
+                break
+            tree_count += is_tree(x, y)
+        return tree_count
+
+    accumulator = 1
+    for dx, dy in [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]:
+        result = count_trees(dx, dy)
+        print(f"- Right {dx}, down {dy}: {result}")
+        accumulator *= result
+
+    print(f"==> {accumulator}")
+
+
+if __name__ == "__main__":
+    main()
