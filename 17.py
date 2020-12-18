32d3a42d0b13d40a18f8ce6cac499f9875f7ad34
Hans <hans.schuelein@stud-mail.uni-wuerzburg.de>
2020-12-18 12:00:37 +0100

    day 17 cleanup

--- 17.py
+++ 17.py
@@ -11,114 +11,7 @@ import aoc
 ADDITIONAL_DIMENSION_NAMES = "zwabcdefghijklmnopqrstuv"
 
 
-class World:
-    world: Dict[int, Dict[int, Set[int]]]
-
-    def __init__(self, board: str = ""):
-        self.world = {}
-        for x, line in enumerate(board.split("\n")):
-            for y, character in enumerate(line):
-                if character == "#":
-                    self[x, y, 0] = True
-
-    def __getitem__(self, coordinate: Tuple[int, int, int]) -> bool:
-        x, y, z = coordinate
-        if (layer := self.world.get(z)) is not None:
-            if (line := layer.get(y)) is not None:
-                return x in line
-        return False
-
-    def __setitem__(self, coordinate: Tuple[int, int, int], value: bool) -> None:
-        x, y, z = coordinate
-        if value:
-            if z not in self.world:
-                self.world[z] = {}
-            layer = self.world[z]
-            if y not in layer:
-                layer[y] = set()
-            layer[y].add(x)
-        else:
-            if (layer := self.world.get(z)) is not None:
-                if (line := layer.get(y)) is not None:
-                    if x in line:
-                        line.remove(x)
-
-    def next(self) -> World:
-        new_world = World()
-        for x in range(self.minx - 1, self.maxx + 2):
-            for y in range(self.miny - 1, self.maxy + 2):
-                for z in range(self.minz - 1, self.maxz + 2):
-                    new_world[x, y, z] = self.next_at(x, y, z)
-        return new_world
-
-    def next_at(self, x: int, y: int, z: int) -> bool:
-        neighbors = self.neighbors(x, y, z)
-        return neighbors == 3 or (neighbors == 2 and self[x, y, z])
-
-    def neighbors(self, x: int, y: int, z: int) -> int:
-        return sum(
-            (x_ != x or y_ != y or z_ != z) and self[x_, y_, z_]
-            for x_ in [x - 1, x, x + 1]
-            for y_ in [y - 1, y, y + 1]
-            for z_ in [z - 1, z, z + 1]
-        )
-
-    @property
-    def minz(self) -> int:
-        return min(self.world.keys())
-
-    @property
-    def maxz(self) -> int:
-        return max(self.world.keys())
-
-    @property
-    def miny(self) -> int:
-        return min([key for layer in self.world.values() for key in layer.keys()])
-
-    @property
-    def maxy(self) -> int:
-        return max([key for layer in self.world.values() for key in layer.keys()])
-
-    @property
-    def minx(self) -> int:
-        return min(
-            [
-                key
-                for layer in self.world.values()
-                for line in layer.values()
-                for key in line
-            ]
-        )
-
-    @property
-    def maxx(self) -> int:
-        return max(
-            [
-                key
-                for layer in self.world.values()
-                for line in layer.values()
-                for key in line
-            ]
-        )
-
-    @property
-    def count(self) -> int:
-        return sum(
-            len(line) for layer in self.world.values() for line in layer.values()
-        )
-
-    def __repr__(self) -> str:
-        output = ""
-        for z in range(self.minz, self.maxz + 1):
-            output += f"\nz={z}\n"
-            for x in range(self.minx, self.maxx + 1):
-                for y in range(self.miny, self.maxy + 1):
-                    output += "#" if self[x, y, z] else "."
-                output += "\n"
-        return output[1:-1]
-
-
-class WorldX(ABC):
+class World(ABC):
     world: Dict[int, Dict[int, Set[int]]]  # default type hint for 3d
     # https://github.com/python/typing/issues/513
     # https://github.com/python/mypy/issues/3345
@@ -283,7 +176,27 @@ class WorldX(ABC):
         )
 
 
-class World3D(WorldX):
+def main_verbose() -> None:
+    string = aoc.get_str(17).strip()
+    # string = ".#.\n..#\n###"
+    world = World3D(string)
+    print(world)
+    for cycle in range(1, 7):
+        world = world.next()
+        print(f"\n==================== Cycle {cycle}\n")
+        print(world)
+    print(world.count)
+
+    world = World4D(string)
+    print(world)
+    for cycle in range(1, 7):
+        world = world.next()
+        print(f"\n==================== Cycle {cycle}\n")
+        print(world)
+    print(world.count)
+
+
+class World3D(World):
     keep = [2, 3]
     enable = [3]
     dimensions = 3
@@ -295,21 +208,14 @@ class World4D(World3D):
 
 def main() -> None:
     string = aoc.get_str(17).strip()
-    # string = ".#.\n..#\n###"
     world = World3D(string)
-    # print(world)
     for cycle in range(1, 7):
         world = world.next()
-        # print(f"\n==================== Cycle {cycle}\n")
-        # print(world)
     print(world.count)
 
     world = World4D(string)
-    # print(world)
     for cycle in range(1, 7):
         world = world.next()
-        # print(f"\n==================== Cycle {cycle}\n")
-        # print(world)
     print(world.count)
 
 
